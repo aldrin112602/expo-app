d6668458fc9a97eec43513fceb12c3fccbb4b486
Aldrin Caballero <aldrincaballero94@gmail.com>
2023-02-23 14:58:29 +0800

    react-native-tailwindcss

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import tw from "react-native-tailwindcss";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw("flex-1 justify-center items-center bg-white")}>
+      <Text style={tw("text-2xl font-bold text-primary")}>Hello World!</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#007AFF",
+        success: "#34C759",
+        warning: "#FF9500",
+        error: "#FF3B30",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
